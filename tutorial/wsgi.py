1400ba7cf1636ef1833f54f99f374e8d9af6fa7d
Jatin742 <200111027@hbtu.ac.in>
2023-01-29 18:53:54 +0530

    Task Done

---
+++ tutorial/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for tutorial project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutorial.settings')
+
+application = get_wsgi_application()
